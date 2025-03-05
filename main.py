f268466bb955747d301001a7edc312db3ee04271
younes_qasempour <qasempouryounes@gmail.com>
2025-03-05 16:58:17 +0330

    Finding followers done

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class InstaFollower:
         time.sleep(3)
 
         self.driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
-        time.sleep(5)
+        time.sleep(7)
 
         # Click "Not now" and ignore Save-login info prompt
         save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
@@ -41,7 +41,24 @@ class InstaFollower:
         #     notifications_prompt.click()
 
     def find_followers(self):
-        pass
+        self.driver.get("https://www.instagram.com/germany.explores/followers/")
+        time.sleep(5)
+
+        self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]'
+                                           '/section/main/div/header/section[3]/ul/li[2]/div/a/span').click()
+        time.sleep(5)
+
+        modal_xpath = ("/html/body/div[4]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/"
+                       "div[2]/div")
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as an HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
+
 
     def follow(self):
         pass
